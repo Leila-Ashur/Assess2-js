396125e8326a0d14eabb09387adbb7aece19eb99
leilaashur <leilahashur@gmail.com>
2023-05-01 09:05:50 +0300

    add commit

--- Assessments/assess2.js
+++ Assessments/assess2.js
@@ -14,14 +14,31 @@ const books = [
 //  all availablebooks.
 function getAvailableBooks() {
 
-    
-}
-// Create a function getBooksByAuthor that takes an author's name as an argument and
-// returns an array of all books by that author.
-function getBooksByAuthor(name) {
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+    return books
 
     
 }
+console.log(getAvailableBooks(books));
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBooksByAuthor(name) {
+  let author=[ 
+    {  author: 'J.D. Salinger'},]
+    return author
+
+}
+console.log(getBooksByAuthor( { title: 'The Catcher in the Rye' },));
 // Create a function addNewBook that takes a book object as an argument and adds 
 // it to the library, ensuring that the new book has all required properties 
 // (title, author,publicationYear, and isAvailable).
@@ -43,10 +60,12 @@ console.log( { title: 'River and the source', author: 'Mackenzie Zielger',
 // that the book is not available.
 function checkoutBook(title) {
     for(let book in title){
-        let check=title
-        if(){
+        let check=addNewBook()
+        if (check!=addNewBook){
+            return "The book was not found"
 
         }
+        
     }
     
 }
@@ -55,7 +74,7 @@ function checkoutBook(title) {
 //  the library, the functionshould return a message indicating that the book 
 // does not belong to the library.
 function returnBook(booktitle) {
-    
+   
 }
 
 
