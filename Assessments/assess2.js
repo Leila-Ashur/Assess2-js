653f7594321c86a257e3a61bc9850f19d3da4c1b
leilaashur <leilahashur@gmail.com>
2023-05-01 08:45:25 +0300

    assess commit

---
+++ Assessments/assess2.js
@@ -0,0 +1,61 @@
+
+const books = [
+    { title: 'The Catcher in the Rye', author: 'J.D. Salinger', publicationYear: 1951,
+    isAvailable: true },
+    { title: 'Great Expectations', author: 'Charles Dickens', publicationYear: 1861,
+    isAvailable: false },
+    { title: 'Crime and Punishment', author: 'Fyodor Dostoevsky', publicationYear:
+    1866, isAvailable: true },
+    { title: 'The Great Gatsby', author: 'F. Scott Fitzgerald', publicationYear: 1925,
+    isAvailable: false },
+    { title: 'The Odyssey', author: 'Homer', publicationYear: -800, isAvailable: true },
+    ];
+// 1. Create a function called getAvailableBooks that returns an array of
+//  all availablebooks.
+function getAvailableBooks() {
+
+    
+}
+// Create a function getBooksByAuthor that takes an author's name as an argument and
+// returns an array of all books by that author.
+function getBooksByAuthor(name) {
+
+    
+}
+// Create a function addNewBook that takes a book object as an argument and adds 
+// it to the library, ensuring that the new book has all required properties 
+// (title, author,publicationYear, and isAvailable).
+function addNewBook(title,author,publicationYear) {
+ const library=[]
+ library.push(title)
+ library.push(author)
+ library.push(publicationYear)
+   
+    
+}
+console.log( { title: 'River and the source', author: 'Mackenzie Zielger',
+ publicationYear: 1934 },);
+
+
+//  4. Create a function checkoutBook that takes a book title as an 
+// argument and changesthe book's isAvailable property to false. If the book 
+// is not found in the library, thefunction should return a message indicating 
+// that the book is not available.
+function checkoutBook(title) {
+    for(let book in title){
+        let check=title
+        if(){
+
+        }
+    }
+    
+}
+// 5. Create a function returnBook that takes a book title as an argument and 
+// changes thebook's isAvailable property to true. If the book is not found in
+//  the library, the functionshould return a message indicating that the book 
+// does not belong to the library.
+function returnBook(booktitle) {
+    
+}
+
+
